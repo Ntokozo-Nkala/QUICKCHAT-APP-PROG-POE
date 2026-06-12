c8c7a8717981c54c04207203aa82fa5950c94be5
Mbali Ntokozo Nkala <166023505+Ntokozo-Nkala@users.noreply.github.com>
2026-06-12 20:08:32 +0200

    Update Login.java

--- QUICKCHAT-APP-PROG-POE/src/main/java/com/mycompany/nkala/ntokozo/prog/part2/Login.java
+++ QUICKCHAT-APP-PROG-POE/src/main/java/com/mycompany/nkala/ntokozo/prog/part2/Login.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.nkala.ntokozo.prog.part2;
+package com.mycompany.quickchat.app.prog.poe;
 import java.util.Scanner;
 
 public class Login{
